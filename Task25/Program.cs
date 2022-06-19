39499d876f054393d0d32f98c11a684c0f22ea98
Ekaterina <pechnikova.07@mail.ru>
2022-06-19 21:02:49 +0300

    Get A In B Power

---
+++ Task25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число А: ");
+int A = Convert.ToInt32(ReadLine());
+Write("Введите число B: ");
+int B = Convert.ToInt32(ReadLine());
+WriteLine();
+
+int answer = GetInPower(A, B);
+WriteLine($"Ответ: {A} в степени {B} = {answer}");
+WriteLine();
+
+int GetInPower(int number, int power)
+{
+    int result = 1;
+    for(int i = 1; i <= power; i++)
+    {
+        result = result * number;
+
+    }
+    return result;  
+}
