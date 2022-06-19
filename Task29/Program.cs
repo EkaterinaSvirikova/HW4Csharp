c92f1376d0957d3e532467208b3f0462d43e7a75
Ekaterina <pechnikova.07@mail.ru>
2022-06-19 22:35:59 +0300

    Array In&Out

---
+++ Task29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int n = int.Parse(ReadLine());
+int[] array = GetRandomArray(n,0,50);
+string answer = String.Join(",", array);
+WriteLine($"Массив [{answer}]");
+
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
